477dba3fc201b8bf74a930fdae0115c038e62a71
Solarmills <iisyssinboshi@gmail.com>
2021-03-04 19:40:08 +0300

    add movement

---
+++ Assets/Scripts/Gameplay_manager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gameplay_manager : MonoBehaviour
+{
+    [SerializeField] private GameObject Button_Group;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Button_Group.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/Ship_controller.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ship_controller : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+        if (Input.GetAxis("Horizontal") != 0)
+        {
+            transform.Rotate(0, 0, Input.GetAxis("Horizontal")*3f);
+        }
+        if (Input.GetAxis("Vertical") != 0)
+        {
+            transform.Translate(0, Input.GetAxis("Vertical") * .1f, 0);
+        }
+    }
+}
