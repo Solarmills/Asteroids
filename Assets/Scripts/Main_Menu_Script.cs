e8de7e5121c17754e7b64fc9ff39c4540abb1224
Solarmills <iisyssinboshi@gmail.com>
2021-03-04 16:20:37 +0300

    initial commit

---
+++ Assets/Scripts/Main_Menu_Script.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Main_Menu_Script : MonoBehaviour
+{
+
+    public void open_game_scene() {
+        SceneManager.LoadSceneAsync(1);    
+    }
+
+    public void close_app() {
+        Application.Quit();
+    }
+}
